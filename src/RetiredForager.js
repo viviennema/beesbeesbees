58789f4f04f88d0733c2ee5fde8877524b93b76f
Sheeva Haghighat <h.sheeva@gmail.com>
2022-06-22 20:51:22 -0700

    Update properties for RetiredForagerBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,11 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.color = "grey";
+  this.canFly = false;
 };
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
 
